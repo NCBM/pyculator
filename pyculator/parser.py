a2432ee7e88bf543176037125a05b19607241d9a
worldmozara <worldmozara@gmail.com>
2022-09-05 18:44:09 +0800

    code more parser.

--- pyculator/parser.py
+++ pyculator/parser.py
@@ -1,23 +1,21 @@
-from dataclasses import dataclass
-from typing import Iterable, List, Tuple
+from decimal import Decimal
+from typing import Any, Iterable, Tuple
 import string
 
 EXPR_LITERAL = 1
 EXPR_OPERATOR = 2
-EXPR_SUFFIX = 4
+EXPR_EQU = 4
 EXPR_ENTER = 8
 EXPR_EXIT = 16
 EXPR_SEP = 32
-EXPR_EQU = 64
 EXPR_UNKNOWN = 1024
 
-literal = string.ascii_lowercase + string.digits + "._"
+literal = string.ascii_lowercase + string.digits + "._@%"
 operator = "+-*/^!"
 equ = "=<>"
-suffix = "@%"
 enter = "([{"
 eexit = ")]}"
-expr_prio = [suffix, "^!", "*/", "+-"]
+expr_prio = ["^!", "*/", "+-"]
 
 
 class Stack:
@@ -38,22 +36,24 @@ class Stack:
     def pop(self):
         return self.data.pop(-1)
 
+    def pop_all(self):
+        while len(self):
+            yield self.pop()
+
 
 def exprtype(c: str):
     if c in literal:
         return EXPR_LITERAL
     elif c in operator:
         return EXPR_OPERATOR
-    elif c in suffix:
-        return EXPR_SUFFIX
+    elif c in equ:
+        return EXPR_EQU
     elif c in enter:
         return EXPR_ENTER
     elif c in eexit:
         return EXPR_EXIT
     elif c == ",":
         return EXPR_SEP
-    elif c in equ:
-        return EXPR_EQU
     else:
         return EXPR_UNKNOWN
 
@@ -65,10 +65,23 @@ def getexprprio(c: str):
     return -1
 
 
-@dataclass
-class ParsedCell:
-    oprt: str
-    oprds: List[str]
+class LiteralAnalyzer:
+    def __init__(self) -> None:
+        pass
+
+    def _analyze_number(self, lit: str):
+        try:
+            if "." not in lit:
+                return int(lit, base=0)
+            else:
+                return Decimal(lit)
+        except ValueError:
+            if "@" in lit:
+                num, bs = lit.split("@", 1)
+                return int(num, base=int(bs))
+            elif lit.endswith("%"):
+                return Decimal(lit[:-1]) / 100
+            raise
 
 
 class Parser:
@@ -94,15 +107,20 @@ class Parser:
 
     def _rpn(self, data: Iterable[Tuple[str, int]]):
         oprts = Stack()
-        for s, t in data:
-            if t == EXPR_LITERAL:
-                yield s, t
-            elif t & (EXPR_OPERATOR | EXPR_SUFFIX):
-                if len(oprts) == 0 or \
-                        getexprprio(oprts.top[0]) >= getexprprio(s):
-                    oprts.top = s, t
-                else:
-                    yield s, t
 
-        while len(oprts) != 0:
-            yield oprts.pop()
+        for s, t in data:
+            if t & EXPR_LITERAL:
+                yield s, t
+            elif t & EXPR_OPERATOR:
+                if len(oprts) and \
+                        getexprprio(oprts.top[0]) < getexprprio(s):
+                    yield from oprts.pop_all()
+                oprts.top = s, t
+
+        yield from oprts.pop_all()
+
+    def parse(self, expr: str):
+        return list(self._rpn(self._trail(expr)))
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.parse(*args, **kwds)
---
+++ pyculator/rpncalc.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+
+class RPNCalculator:
+    def __init__(self) -> None:
+        pass
+
+    def _calc(self, data: Tuple[str, int]):
+        ...
